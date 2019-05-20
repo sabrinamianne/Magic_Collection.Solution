058bc4aab5ac39b7b3300117d3960a879c2ca9a6
Student <example@example.com>
2019-05-20 12:21:27 -0700

    add deck img, droppable target working (can i increase the radius/area?), add new 'card' css class

--- Magic_Collection/wwwroot/js/scripts.js
+++ Magic_Collection/wwwroot/js/scripts.js
@@ -3,5 +3,10 @@ $().ready(function() {
 
     $(".draggable").draggable({revert: true});
 
+    $(".droppable").droppable({
+        classes: {"ui-droppable-hover" : "drop-hover"},
+        drop: function(){
+            console.log("dropped on target");
+    }});
 });
 
